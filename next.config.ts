e26248fc00d5e4928924712185adbc36ee9cef0c
xya0526 <xya0526c@gmail.com>
2026-08-25 16:47:25 +0800

    chore: 更新项目配置与依赖
    更新环境变量模板、Next.js 配置、新增 prisma/radix-ui/redis/jose 等依赖

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+     allowedDevOrigins: ['sj.frp.one','xg-2.frp.one'],
+
+};
 
 export default nextConfig;
